8b68af0cd5ee79b35c0f63b50785580835825e5c
sofi131 <sofiasanmartin131@gmail.com>
2024-04-25 18:39:02 +0200

    cambios sql

--- appOrder.sql
+++ appOrder.sql
@@ -1,198 +1,228 @@
-CREATE DATABASE  IF NOT EXISTS `ceica` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `ceica`;
--- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: ceica
--- ------------------------------------------------------
--- Server version	8.2.0
+-- Servidor: localhost:3306
+-- Tiempo de generación: 25-04-2024 a las 15:54:04
+-- Versión del servidor: 10.5.20-MariaDB
+-- Versión de PHP: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `address`
+-- Base de datos: `id22078452_ceica`
 --
 
-DROP TABLE IF EXISTS `address`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `address`
+--
+
 CREATE TABLE `address` (
-  `idaddress` int NOT NULL AUTO_INCREMENT,
+  `idaddress` int(11) NOT NULL,
   `street` varchar(255) DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
-  `zipcode` varchar(255) DEFAULT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  `iduser` int NOT NULL,
-  PRIMARY KEY (`idaddress`),
-  KEY `fk_address_user1_idx` (`iduser`),
-  CONSTRAINT `fk_address_user1` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `zipcode` varchar(45) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `address`
+-- Estructura de tabla para la tabla `cart`
 --
 
-LOCK TABLES `address` WRITE;
-/*!40000 ALTER TABLE `address` DISABLE KEYS */;
-/*!40000 ALTER TABLE `address` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `cart`
---
-
-DROP TABLE IF EXISTS `cart`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cart` (
-  `idcart` int NOT NULL AUTO_INCREMENT,
-  `date` datetime DEFAULT CURRENT_TIMESTAMP,
-  `iduser` int NOT NULL,
-  PRIMARY KEY (`idcart`),
-  KEY `fk_cart_user_idx` (`iduser`),
-  CONSTRAINT `fk_cart_user` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `idcart` int(11) NOT NULL,
+  `date` datetime DEFAULT current_timestamp(),
+  `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `cart`
+-- Estructura de tabla para la tabla `cart_detail`
 --
 
-LOCK TABLES `cart` WRITE;
-/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `cart_detail` (
+  `idcartdetail` int(11) NOT NULL,
+  `idcart` int(11) NOT NULL,
+  `idproduct` int(11) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `cart_details`
+-- Estructura de tabla para la tabla `order`
 --
 
-DROP TABLE IF EXISTS `cart_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `cart_details` (
-  `idcartdetails` int NOT NULL AUTO_INCREMENT,
-  `idcart` int NOT NULL,
-  `idproduct` int NOT NULL,
-  `quantity` int DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`idcarddetails`),
-  KEY `fk_card_details_cart1_idx` (`idcart`),
-  KEY `fk_card_details_product1_idx` (`idproduct`),
-  CONSTRAINT `fk_card_details_cart1` FOREIGN KEY (`idcart`) REFERENCES `cart` (`idcart`),
-  CONSTRAINT `fk_card_details_product1` FOREIGN KEY (`idproduct`) REFERENCES `product` (`idproduct`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cart_details`
---
-
-LOCK TABLES `cart_details` WRITE;
-/*!40000 ALTER TABLE `cart_details` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cart_details` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `order`
---
-
-DROP TABLE IF EXISTS `order`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `order` (
-  `idorder` int NOT NULL AUTO_INCREMENT,
+  `idorder` int(11) NOT NULL,
   `date` datetime DEFAULT NULL,
-  `delivery_date` datetime DEFAULT NULL,
-  `idcart` int NOT NULL,
-  `idaddress` int NOT NULL,
-  PRIMARY KEY (`idorder`),
-  KEY `fk_order_cart1_idx` (`idcart`),
-  KEY `fk_order_address1_idx` (`idaddress`),
-  CONSTRAINT `fk_order_address1` FOREIGN KEY (`idaddress`) REFERENCES `address` (`idaddress`),
-  CONSTRAINT `fk_order_cart1` FOREIGN KEY (`idcart`) REFERENCES `cart` (`idcart`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `deliverydate` datetime DEFAULT NULL,
+  `idcart` int(11) NOT NULL,
+  `idaddress` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `order`
+-- Estructura de tabla para la tabla `product`
 --
 
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `product`
---
-
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
-  `idproduct` int NOT NULL AUTO_INCREMENT,
+  `idproduct` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `image` varchar(45) DEFAULT NULL,
-  `quantity` int DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`idproduct`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `image` varchar(255) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `product`
+-- Estructura de tabla para la tabla `user`
 --
 
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `iduser` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
-  `image` varchar(255) COLLATE utf8mb3_spanish2_ci DEFAULT NULL,
-  PRIMARY KEY (`iduser`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_spanish2_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `iduser` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
 
 --
--- Dumping data for table `user`
+-- Índices para tablas volcadas
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'sofia','sofia@gmail.com','$2y$10$T9C6Yp8LuhW5oTjSMLiBxO0UFRaV4oaWzJ3tOgAN5WeKfGjPfyKEG','ceica1.png'),(2,'paco','paco@gmail.com','12345','icons8-asterisco-24.png'),(3,'maria','maria@gmail.com','12345','ceica.png');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Indices de la tabla `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`idaddress`),
+  ADD KEY `fk_addres_user1_idx` (`iduser`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indices de la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`idcart`),
+  ADD KEY `fk_cart_user_idx` (`iduser`);
+
+--
+-- Indices de la tabla `cart_detail`
+--
+ALTER TABLE `cart_detail`
+  ADD PRIMARY KEY (`idcartdetail`),
+  ADD KEY `fk_cart_detail_cart1_idx` (`idcart`),
+  ADD KEY `fk_cart_detail_product1_idx` (`idproduct`);
+
+--
+-- Indices de la tabla `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`idorder`),
+  ADD KEY `fk_order_cart1_idx` (`idcart`),
+  ADD KEY `fk_order_addres1_idx` (`idaddress`);
+
+--
+-- Indices de la tabla `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`idproduct`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`iduser`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `address`
+--
+ALTER TABLE `address`
+  MODIFY `idaddress` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `idcart` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cart_detail`
+--
+ALTER TABLE `cart_detail`
+  MODIFY `idcartdetail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `order`
+--
+ALTER TABLE `order`
+  MODIFY `idorder` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `product`
+--
+ALTER TABLE `product`
+  MODIFY `idproduct` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `address`
+--
+ALTER TABLE `address`
+  ADD CONSTRAINT `fk_addres_user1` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `fk_cart_user` FOREIGN KEY (`iduser`) REFERENCES `user` (`iduser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `cart_detail`
+--
+ALTER TABLE `cart_detail`
+  ADD CONSTRAINT `fk_cart_detail_cart1` FOREIGN KEY (`idcart`) REFERENCES `cart` (`idcart`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_cart_detail_product1` FOREIGN KEY (`idproduct`) REFERENCES `product` (`idproduct`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `fk_order_addres1` FOREIGN KEY (`idaddress`) REFERENCES `address` (`idaddress`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_order_cart1` FOREIGN KEY (`idcart`) REFERENCES `cart` (`idcart`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-04-25 17:34:58
