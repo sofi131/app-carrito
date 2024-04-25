62323731500638376594174c17e2f736152ea785
sofi131 <sofiasanmartin131@gmail.com>
2024-04-25 20:00:18 +0200

    form

--- index.php
+++ index.php
@@ -37,7 +37,12 @@ $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
                 <h5 class="card-title">' . $product["price"] . '€/kg</h5>
             </div>
           </div>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+
+          <div>
+          <input class="form-control" type="number" name="" id="">
+          </div>
+
+          <a href="#" class="btn btn-primary">Add to Cart</a>
         </div>
       </div>';
     }
@@ -47,7 +52,7 @@ $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+   
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
