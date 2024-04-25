68e2a756adae04d6b4b482be7ae6ae3f82cf43d3
sofi131 <sofiasanmartin131@gmail.com>
2024-04-25 19:32:02 +0200

    index e imágenes

--- conexion.php
+++ conexion.php
@@ -14,7 +14,7 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Salida si la conexión es exitosa
-    echo "Conexión exitosa a la base de datos '$database'";
+    //echo "Conexión exitosa a la base de datos '$database'";
 } catch (PDOException $e) {
     // Salida si hay un error en la conexión
     echo "Error en la conexión: " . $e->getMessage();
