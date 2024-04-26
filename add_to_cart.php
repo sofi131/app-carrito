1004ae1c285224846217aeba3116557a1bbe1076
sofi131 <sofiasanmartin131@gmail.com>
2024-04-26 16:51:00 +0200

    guardar en sesión los objetos

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,13 +1,22 @@
 <?php
+include_once("./models/product.php");
 session_start();
 
 if (isset($_GET["idproduct"])) {
     $idproduct = $_GET["idproduct"];
     $quantity = isset($_GET["quantity"]) ? $_GET["quantity"] : 1;
     //comprobamos si el usuario se ha logueado
-    if(isset($_SESSION["user"])){
+    if (isset($_SESSION["user"])) {
         //guardamos en bbdd
-    }else{
+    } else {
         //guardamos el carrito en session
+        $product = new Product($idproduct, $quantity);
+        if (isset($_SESSION["cart"])) {
+            $cart = $_SESSION["cart"];
+        } else {
+            $cart = array();
+        }
+        array_push($cart, $product);
+        $_SESSION["cart"]=$cart;
     }
 }
