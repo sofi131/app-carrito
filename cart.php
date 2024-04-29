f651429a8a1dbc24e26ac97e4cf655ca8b306e71
sofi131 <sofiasanmartin131@gmail.com>
2024-04-29 19:02:45 +0200

    cart y product imagen

--- cart.php
+++ cart.php
@@ -4,11 +4,26 @@ session_start();
 if (isset($_SESSION["Username"])) {
     if (isset($_SESSION["cart"])) {
         //Existe usuario y carrito en session
-        $user=$_SESSION["Username"];
-        $cart=$_SESSION["cart"];
+        $user = $_SESSION["Username"];
+        $cart = $_SESSION["cart"];
         //consultamos información de los productos a la bbdd
         require_once("conexion.php");
-        
+        //recorremos el array de artículos que hay en el carrito
+        foreach ($cart as $product) {
+            # consulta
+            $sql = "select * from product where idproduct=?";
+            $stm = $conn->prepare($sql);
+            $stm->bindParam(1, $product->idproduct);
+            $stm->execute();
+            //Comprobamos si hay algún registro - esto es un bucle
+            if($stm->rowCount()>0){
+                $result=$stm->fetchAll(PDO::FETCH_ASSOC);
+                $product->name=$result[0]["name"];
+                $product->description=$result[0]["description"];
+                $product->price=$result[0]["price"];
+                $product->image=$result[0]["image"];
+            }
+        }
     } else {
         header("Location: ./");
         exit();
